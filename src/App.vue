ab137f486da8f84abcd871557016ca5a62a9bcf5
Flavio Nogueira <flavionogueirabarros@gmail.com>
2020-09-21 23:28:25 -0300

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Contador :valorInicial="10"/>
+    <ContadorClasse :valorInicial="10"/>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from 'vue-property-decorator'
+import Contador from './componentes/contatador.vue'
+import ContadorClasse from './componentes/contadorClasse.vue'
+
 
 @Component({
   components: {
-    HelloWorld,
+    Contador,
+    ContadorClasse
   },
 })
 export default class App extends Vue {}
---
+++ src/componentes/contadorClasse.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>Contador Classe</h1>
+        <h2>{{ valor }}</h2>
+        <h2>{{ParOuImpar}}</h2>
+        <button @click="setValor(-1)">Dec</button>
+        <button @click="setValor(1)">Inc</button>
+    </div>
+    
+</template>
+
+
+<script lang="ts">
+import {Vue, Component, Prop} from 'vue-property-decorator'
+import { extend } from 'vue/types/umd'
+
+@Component
+export default class ContadorClasse extends Vue{
+    @Prop(Number) private readonly valorInicial?: number
+    private valor:number = this.valorInicial || 0
+    
+    public setValor(delta:number){
+        this.valor += delta
+    }
+
+    public get ParOuImpar(): string {
+        return this.valor % 2 === 0 ? 'Par' : 'Impar'
+    }
+}
+</script>
+
+<style>
+
+</style>
